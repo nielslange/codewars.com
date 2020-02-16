7c1adc5b9adcb4c24c99027c23558d2f3baebfdd
Niels Lange <info@nielslange.de>
2020-02-16 21:35:35 +0700

    Update orderWeight.php

--- orderWeight.php
+++ orderWeight.php
@@ -16,5 +16,5 @@ function orderWeight($str) {
 	return implode(' ', array_column($array, 'key'));
 }
 
-var_dump(orderWeight("103 123 4444 99 2000")); 													// Expected: 2000 103 123 4444 99
+var_dump(orderWeight("103 123 4444 99 2000"));							// Expected: 2000 103 123 4444 99
 var_dump(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123")); // Expected: 11 11 2000 10003 22 123 1234000 44444444 9999
